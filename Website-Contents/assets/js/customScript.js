161d81fc946dfaadaec4eada0afe6026db2833c3
Somtoya Arinze <Somtoyaarinze@gmail.com>
2017-07-18 01:57:39 -0700

    fomating updates, no javascript needed

--- Website-Contents/assets/js/customScript.js
+++ Website-Contents/assets/js/customScript.js
@@ -1,15 +1,19 @@
+/*
 // Get the modal
-var modal = document.getElementById('myModal');
+var modal = document.getElementById('myModal1');
 
 // Get the image and insert it inside the modal - use its "alt" text as a caption
 var img = document.getElementById('myImg');
 var modalImg = document.getElementById("vid1");
-var captionText = document.getElementById("caption");
+*/
+var captionText = document.getElementsByClassName("caption");
+
 img.onclick = function(){
     modal.style.display = "block";
-    modalImg.src = this.src;
+    //modalImg.src = this.src;
     captionText.innerHTML = this.alt;
 }
+
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
